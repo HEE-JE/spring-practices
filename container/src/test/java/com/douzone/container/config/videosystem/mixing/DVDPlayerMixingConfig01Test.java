a9306f04837652ac1f1f6b99c74df2988119ca87
HEE-JE <windba78@naver.com>
2022-06-16 13:55:28 +0900

    Java Config Mixing

---
+++ container/src/test/java/com/douzone/container/config/videosystem/mixing/DVDPlayerMixingConfig01Test.java
@@ -0,0 +1,21 @@
+package com.douzone.container.config.videosystem.mixing;
+
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import com.douzone.container.videosystem.DVDPlayer;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = { DVDPlayerConfig.class })
+public class DVDPlayerMixingConfig01Test {
+	@Autowired
+	private DVDPlayer dvdPlayer;
+
+	public void testPlayerNotNull() {
+		assertNotNull(dvdPlayer);
+	}
+}
